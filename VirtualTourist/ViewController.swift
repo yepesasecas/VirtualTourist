8003ba7a97544785f095fce5372a295150e81f01
Andres Yepes <yepes07@gmail.com>
2018-12-11 21:53:23 -0500

    Initial Commit

---
+++ VirtualTourist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VirtualTourist
+//
+//  Created by Andres Yepes on 12/11/18.
+//  Copyright © 2018 Andres Yepes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
